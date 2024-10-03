0653f4141a19dfe4c177450ccbcd9969e62a3801
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-10-04 00:16:18 +0600

    Адаптация внешнего вида моделей под конкретно-прикладную задачу

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']     # отображаемые на странице списка постов поля
+    list_filter = ['status', 'created', 'publish', 'author']     # страница списка содержит правую боковую панель, которая позволяет фильтровать результаты по полям, включенным в атрибут list_filter
+    search_fields = ['title', 'body']   # определили список полей, по которым можно выполнять поиск
+    prepopulated_fields = {'slug': ('title',)}   # сообщили Django, что нужно предзаполнять поле slug данными, вводимыми в поле title
+    raw_id_fields = ['author']   # поле author отображается поисковым виджетом, который будет более приемлемым, чем выбор из выпадающего списка, когда у вас тысячи пользователей.
+    autocomplete_fields = ['author']   # autocomplete_fields  - заменяет выпадающий список на поле с автозаполнением,
+    date_hierarchy = 'publish'   # навигационные ссылки для навигации по иерархии дат
+    ordering = ['status', 'publish']   # по умолчанию посты упорядочены по столбцам 'status', 'publish'
