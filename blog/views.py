9e045796aa5c13e05416634a950f67059604f3f4
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-10-11 01:10:06 +0600

    Создание дружественных для поисковой оптимизации URL-адресов

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
@@ -10,13 +10,13 @@ def post_list(request):
                   {'posts': posts})
 
 
-def post_detail(request, id): 
-    try:
-        post = Post.published.get(id=id)
-
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
-
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
