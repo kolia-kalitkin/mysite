e87b09a91dd10bc70c98bcd95abe7ed9509a13fe
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-10-19 02:08:26 +0600

    Добавление комментариев

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Post
+from .models import Post, Comment
 
 
-from django.contrib import admin
-from .models import Post
-
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
+    
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
