03629c7d426a2db5ef880367e9ddbab69e3cc104
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-10-19 14:57:37 +0600

    категоризация постов посредством тегов(добавление тегирования)

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-
+from taggit.models import Tag
 
 class PostListView(ListView):
     """
@@ -17,20 +17,31 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
     
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
     except EmptyPage:
         # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
+        # выдать последнюю страницу результатов
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
