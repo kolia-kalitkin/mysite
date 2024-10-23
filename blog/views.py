95e762104920b6af8439feddb69e17bc4afb8e8b
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-10-24 00:14:46 +0600

    Выделение основ слов и ранжирование результатов

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import SearchVector
 from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class PostListView(ListView):
     """
@@ -134,9 +135,14 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            # Для того чтобы использовать другую конфигурацию поиска, в SearchVector и SearchQuery передается атрибут config. 
+            # Такой подход позволяет использовать разные языковые парсеры и словари.
+            search_vector = SearchVector('title', 'body', config='russian')     # выделяются основы слов и удаляются стоп-слова на русском языке 
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
