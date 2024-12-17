e66605f1d1c4a0ca6f2ad6cb6e3b7939c458f25a
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-12-18 01:44:51 +0600

    Фильтрация по текущему аутентифицированному пользователю, сделавшему  запрос

--- blog_api/views.py
+++ blog_api/views.py
@@ -2,9 +2,14 @@ from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
 
+
 class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user    # отфильтровать набор запросов, чтобы гарантировать, что будут возвращены только результаты, относящиеся к текущему аутентифицированному пользователю, сделавшему запрос.
+        return Post.objects.filter(author=user)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
