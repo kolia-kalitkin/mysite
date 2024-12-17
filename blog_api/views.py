9bbfbbae59437e1b5b762cbbf6aa99890a3a8aa9
Андрей Седельников <nikolay.kalitkin@gmail.com>
2024-12-18 03:22:33 +0600

    Фильтрация по URL

--- blog_api/views.py
+++ blog_api/views.py
@@ -4,14 +4,19 @@ from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
-    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get_queryset(self):
-        user = self.request.user    # отфильтровать набор запросов, чтобы гарантировать, что будут возвращены только результаты, относящиеся к текущему аутентифицированному пользователю, сделавшему запрос.
-        return Post.objects.filter(author=user)
-
-
+    
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserPostList(generics.ListAPIView):
+    """представление, которое возвращает набор постов, отфильтрованный по id пользователя в части URL"""
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.kwargs['id']
+        return Post.objects.filter(author=user)
